1c887ae1f1dce1188f705e9a6a1dff297d1c4095
Abanob Tawfik <32289746+AbanobTawfik@users.noreply.github.com>
2019-03-26 21:20:30 +1100

    Update a1.sql

--- assignment1/a1.sql
+++ assignment1/a1.sql
@@ -640,7 +640,7 @@ END; $$ language plpgsql;
 -- gain for that sector on that day, or the lower than the minimum gain for that
 -- sector on that day
 CREATE TRIGGER Q17
-AFTER UPDATE ON asx
+AFTER INSERT ON asx
 FOR EACH ROW EXECUTE PROCEDURE starUpdateTrigger();
 
 --------------------------------------------------------------------------------
