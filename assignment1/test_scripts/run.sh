4ceebbcf90efbb706912f3a1933195437732c02b
Abanob Tawfik <32289746+AbanobTawfik@users.noreply.github.com>
2019-03-26 21:43:29 +1100

    Update run.sh

--- assignment1/test_scripts/run.sh
+++ assignment1/test_scripts/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+ssh grieg
+read -s "zid: (include the z)" zid
+source /srvr/$zid/env
 
+pgs start
 dropdb a1-check > output.txt
 createdb a1-check >> output.txt
 psql a1-check -f asx-schema.sql
@@ -8,3 +12,4 @@ psql a1-check -f asx-insert.sql
 psql a1-check -f a1.sql
 psql -d a1-check -f input.txt >> output.txt
 cat output.txt
+pgs stop
