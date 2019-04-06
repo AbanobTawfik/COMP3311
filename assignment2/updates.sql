a66442090c5c86dbac1fbc5873f2a54095f4065f
Abanob Tawfik <z5075490@ad.unsw.edu.au>
2019-04-06 12:26:44 +1100

    task A commented and completed

--- assignment2/updates.sql
+++ assignment2/updates.sql
@@ -4,27 +4,27 @@
 --
 -- Written by Abanob Tawfik (z5075490), Apr 2019
 
---  This script takes a "vanilla" imdb database (a2.db) and
---  make all of the changes necessary to make the database
---  work correctly with your PHP scripts.
---  
---  Such changes might involve adding new views,
---  PLpgSQL functions, triggers, etc. Other changes might
---  involve dropping or redefining existing
---  views and functions (if any and if applicable).
---  You are not allowed to create new tables for this assignment.
---  
---  Make sure that this script does EVERYTHING necessary to
---  upgrade a vanilla database; if we need to chase you up
---  because you forgot to include some of the changes, and
---  your system will not work correctly because of this, you
---  will lose half of your assignment 2 final mark as penalty.
---
+-- This script takes a "vanilla" imdb database (a2.db) and
+-- make all of the changes necessary to make the database
+-- work correctly with the PHP scripts.
 
---  This is to ensure that there is no trailing spaces in movie titles,
---  as some tasks need to perform full title search.
+
+-- This is to ensure that there is no trailing spaces in movie titles,
+-- As some tasks need to perform full title search.
 UPDATE movie SET title = TRIM (title);
 
+--------------------------------------------------------------------------------
+--                                TASK A                                      --
+--------------------------------------------------------------------------------
+-- This query will be used to return all the movies actors have acted in and
+-- All the details of the movies associated. This query will be used to solve
+-- Task A by assosciating the actor with all the movies they have worked in
+-- And all details of the movie.
+
+-- This query uses multiple joins to link data between multiple tables. We want
+-- To try link all the movies to the actors and all the details of that movie
+-- Including the director name from the director table, and rating from the
+-- Rating table
 CREATE OR REPLACE VIEW ACTORS_MOVIE(actor, movie, director, year, tv_rating,
         rating) as
     SELECT actor_movie_id.actor_name,
@@ -33,19 +33,23 @@ CREATE OR REPLACE VIEW ACTORS_MOVIE(actor, movie, director, year, tv_rating,
            movie_list.year,
            movie_list.content_rating,
            rating_list.imdb_score
+    -- First we want to create a subquery to return the movie ids an actor has
+    -- Acted in, we want to join first the actor name and movie id from the
+    -- Actor table and acting table.
     FROM (SELECT actor_list.name as actor_name,
                  acting_list.movie_id as movie_id
           FROM actor actor_list
                INNER JOIN acting acting_list
                           ON acting_list.actor_id = actor_list.id) actor_movie_id
+         -- Join the subquery with the movie table with matching movie id's
          INNER JOIN movie movie_list
                     ON movie_list.id = actor_movie_id.movie_id
+         -- Join aswell on the rating table with matching movie id
          INNER JOIN rating rating_list
                     ON movie_list.id = rating_list.movie_id
+         -- Join aswell on the director table with the matching director id
          INNER JOIN director director_list
                     ON director_list.id = movie_list.director_id
+    -- order chronologically by the name, and then year
     ORDER BY actor_movie_id.actor_name, movie_list.year;
 
-
---  Add your code below
---
